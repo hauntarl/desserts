7d70695af20c85d90f0c275bf9b7bc11cc56162c
hauntarl <sameer.mungole@gmail.com>
2024-03-19 06:50:12 -0500

    Configure custom URLCache

--- Desserts/DessertsApp.swift
+++ Desserts/DessertsApp.swift
@@ -7,8 +7,29 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        // Configure URLCache
+        let memoryCapacity = 50 * 1024 * 1024  // 50 MB
+        let diskCapacity = 150 * 1024 * 1024  // 150 MB
+        let cacheDirectory = "DessertsCache"
+        let urlCache = URLCache(
+            memoryCapacity: memoryCapacity,
+            diskCapacity: diskCapacity,
+            diskPath: cacheDirectory
+        )
+        URLCache.shared = urlCache
+        return true
+    }
+}
+
 @main
 struct DessertsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             DessertsView()
